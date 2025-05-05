471e95c1813ed5d554e894ea0f0e45dc2bd892ca
riswandaP <riswandaputra03@gmail.com>
2025-05-05 11:35:02 +0700

    memberikan friend function

--- friendFunctionDua.cpp
+++ friendFunctionDua.cpp
@@ -9,4 +9,12 @@ class manusia
         // void showNilaiPelajar(pelajar &x); 
 };
 
-
+class pelajar
+{
+    private:
+        int nilai;
+    
+    public:
+        pelajar() { nilai = 100; }
+        friend void manusia::showNilaiPelajar(pelajar &x);
+}
